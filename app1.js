51f413fdcd1a1e844b90a49e6bab252f5299561b
sufiyan2401 <110626687+sufiyan2401@users.noreply.github.com>
2022-09-03 01:57:52 +0500

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,114 @@
+// ALERTS // 
+// alert("This is Assignment 1 ")
+// x---------x-------x//
+
+// VARIABLES FOR STRING // 
+// var a = "Muhammad Sufiyan"
+// var b = "Age: 15 "
+// var c = "Learning Web-development from jawan pakistan"
+// var d = "Email address: Sufiyan 240107@gmail.com"
+// alert(a)
+// alert(b) 
+// alert(c)
+// alert(d) 
+// x---------x-------x//
+
+// VARIABLES FOR NUMBERS // 
+// var a = "ASSIGNMENT 3"
+// document.write(a);
+// alert(a)
+
+// var b = "  I make this web in 5 minutes"
+// document.write(b);
+// alert(b)
+// x---------x-------x//
+
+// VARIABLES NAME:ILLEGAL & LEGAL // 
+// document.write("Rules for naming JS VARIABLES <br/> <br/> Variables names can only contain . numbers$or_For example : Smy_1Variable <br/> Variable must begin with a letter$ or _ for example : $name _name or name <br/> Variable names are case sensitive <br/>Variable names should not be JS keywords");
+// x---------x-------x//
+
+// MATH EXPRESSIONS  // 
+// var num=prompt("Enter a number");
+// for(var i=1;i<=10;i++){
+//     document.write("<div>");
+//     document.write(num + "x" + i + "=" +i*num);
+//     document.write("</div>");
+// }
+// x---------x-------x//
+// MATH EXPRESSIONS  // 
+// var a = prompt("Enter a number"), b = prompt("Enter a  second number");
+// var result = --a - --b + ++b + b--;
+// document.write("The answer after addition is    ")
+// document.write(result)
+
+//  MATH EXPRESSIONS ASSIGNMENT 6-9 // 
+// let maths,chem,urdu,history,English;
+//  maths=+prompt("Enter Maths marks" , "Input Number");
+//  chem=+prompt("Enter Chem marks" , "Input Number");
+//  urdu=+prompt("Enter urdu marks" , "Input Number");
+//  history=+prompt("Enter P.S.T marks" , "Input Number");
+//  English=+prompt("Enter english marks" , "Input Number");
+//  hindi=+prompt("enter your Computer marks" , "Input NUMBER")
+
+//  var submarks= maths+chem+urdu+history+English;
+// //  alert(submarks);
+// document.write("Your Total Marks: ")
+//  document.write(submarks)
+
+//  var percentage=(submarks * 100)/500;
+ 
+
+//  if (percentage>90)
+//  {
+//   alert("Your grade is = A+")
+//  }
+//  else if (percentage>80)
+//  {
+//   alert("Your grade is = A")
+//  }
+
+//  else if (percentage>70)
+//  {
+//   alert("Your grade is = B")
+//  }
+
+//  else if (percentage>60)
+//  {
+//   alert("Your grade is = C")
+//  }
+
+//  else if (percentage>50)
+//  {
+//   alert("Your grade is = D")
+//  }
+
+
+//  else if (percentage>40)
+//  {
+//   alert("Your grade is = E")
+//  }
+
+//  else if (percentage<40)
+//  {
+//   alert("Your grade is = F")
+//  }
+//  else{
+//      alert("You inputed wrong values");
+//  }
+
+// USER INPUT & CONDITIONAL STATEMENTS  // 
+// prompt("Enter The Name of city ")
+// if ("Karachi")
+//  {
+//    alert("Welcome To the city of lights")
+//   }
+// else if ("Lahore")
+//   {
+//     alert("Welcome To the Lahore")
+//    }
+// else  ("Islamabad")
+//    {
+//      alert("Welcome To the Islamabad")
+//     }
+    // xxxxxxxxx---------xxxxxx
+
